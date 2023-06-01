d8931da4dc3781efe1e33450753f77f0737db9f4
MaxDimas <maxdimas@outlook.com>
2023-06-01 08:33:41 -0600

    feat: exercise three

--- exercises.py
+++ exercises.py
@@ -39,3 +39,33 @@ while index < len(videoGames):
     gameTitle = videoGames[index]
     print(f"Game at index {index}: {gameTitle}")
     index += 1
+
+print("")
+print("______________________________________________________________________")
+print("")
+
+print("Exercise three: Modify a dictionary")
+print("")
+print("")
+
+# We create dictionary
+dictionary = {'name': 'John', 'age': 25, 'city': 'New York'}
+print("Current dictionary:", dictionary)
+print("")
+print("")
+
+# Now we modify the dictionary
+print("Please modify one of the next properties(name, age, or city)")
+propertyName = input("Enter the name of the property to modify: ")
+print("")
+
+if propertyName in dictionary:
+    newValue = input("Enter the new value: ")
+    dictionary[propertyName] = newValue
+    print("")
+    print("Modified dictionary:", dictionary)
+
+# If we dont enter a valid property it will pop an error
+else:
+    print("")
+    print("Error: Property not found in the dictionary.")
