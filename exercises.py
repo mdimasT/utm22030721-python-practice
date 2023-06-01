d655ee14703f20d6c8195567c373a82d4abf81bc
MaxDimas <maxdimas@outlook.com>
2023-06-01 08:30:46 -0600

    feat: second exercise

--- exercises.py
+++ exercises.py
@@ -19,3 +19,23 @@ print("")
 print("List elements using for loop:")
 for item in list:
     print(item)
+
+print("")
+print("______________________________________________________________________")
+print("")
+
+print("Exercise two: tupple with seven items useing while loop")
+
+# Create and print tuple
+videoGames = ("Super Mario Bros", "Valorant", "Minecraft", "CSGO", "Grand Theft Auto V", "Overwatch", "Fortnite")
+index = 0
+print("The video games in the tuple are: ", videoGames)
+print("")
+print("")
+
+# Now useing while loop
+print("Video game titles using a while loop:")
+while index < len(videoGames):
+    gameTitle = videoGames[index]
+    print(f"Game at index {index}: {gameTitle}")
+    index += 1
